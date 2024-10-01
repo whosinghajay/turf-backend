0fc7661ef69e58060cec84de3455b86bb16ca8b7
unknown <whosinghajay@gmail.com>
2024-10-01 18:58:40 +0530

    template id added to env

--- dist/utils/otpUtils.js
+++ dist/utils/otpUtils.js
@@ -8,6 +8,7 @@ const axios_1 = __importDefault(require("axios"));
 const SMSINDORI_USERNAME = process.env.SMSINDORI_USERNAME;
 const SMSINDORI_PASSWORD = process.env.SMSINDORI_PASSWORD;
 const SMSINDORI_SENDER_ID = process.env.SMSINDORI_SENDER_ID;
+const SMSINDORI_TEMPLATE_ID = process.env.SMSINDORI_TEMPLATE_ID;
 // To Generate a 4-digit OTP
 const generateOTP = () => {
     return Math.floor(1000 + Math.random() * 9000).toString();
@@ -17,7 +18,7 @@ const sendOTP = async (phoneNumber, otp) => {
     //   const message = `Your OTP is: ${otp}`;
     console.log(phoneNumber, "number");
     console.log(otp, "otp");
-    const url = `http://sms.smsindori.com/http-api.php?username=${SMSINDORI_USERNAME}&password=${SMSINDORI_PASSWORD}&senderid=${SMSINDORI_SENDER_ID}&route=06&number=${phoneNumber}&message=${otp} is your Login otp for ChatHooter IMRSMS&templateid=1207167594991592693`;
+    const url = `http://sms.smsindori.com/http-api.php?username=${SMSINDORI_USERNAME}&password=${SMSINDORI_PASSWORD}&senderid=${SMSINDORI_SENDER_ID}&route=06&number=${phoneNumber}&message=${otp} is your Login otp for ChatHooter IMRSMS&templateid=${SMSINDORI_TEMPLATE_ID}`;
     try {
         const response = await axios_1.default.get(url);
         return response.data;
--- src/utils/otpUtils.ts
+++ src/utils/otpUtils.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 const SMSINDORI_USERNAME = process.env.SMSINDORI_USERNAME;
 const SMSINDORI_PASSWORD = process.env.SMSINDORI_PASSWORD;
 const SMSINDORI_SENDER_ID = process.env.SMSINDORI_SENDER_ID;
+const SMSINDORI_TEMPLATE_ID = process.env.SMSINDORI_TEMPLATE_ID;
 
 // To Generate a 4-digit OTP
 export const generateOTP = (): string => {
@@ -13,12 +14,12 @@ export const sendOTP = async (phoneNumber: string, otp: string) => {
   //   const message = `Your OTP is: ${otp}`;
   console.log(phoneNumber, "number");
   console.log(otp, "otp");
-    const url = `http://sms.smsindori.com/http-api.php?username=${SMSINDORI_USERNAME}&password=${SMSINDORI_PASSWORD}&senderid=${SMSINDORI_SENDER_ID}&route=06&number=${phoneNumber}&message=${otp} is your Login otp for ChatHooter IMRSMS&templateid=1207167594991592693`;
+  const url = `http://sms.smsindori.com/http-api.php?username=${SMSINDORI_USERNAME}&password=${SMSINDORI_PASSWORD}&senderid=${SMSINDORI_SENDER_ID}&route=06&number=${phoneNumber}&message=${otp} is your Login otp for ChatHooter IMRSMS&templateid=${SMSINDORI_TEMPLATE_ID}`;
 
-    try {
-      const response = await axios.get(url);
-      return response.data;
-    } catch (error) {
-      throw new Error("Failed to send OTP");
-    }
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    throw new Error("Failed to send OTP");
+  }
 };
